21258f97f37ecbbec820a9762648b3a976076028
Pedro Minicz <pedrominicz@gmail.com>
2019-08-29 10:51:10 -0300

    .

---
+++ haskell/euler/001.hs
@@ -0,0 +1,8 @@
+sumMultiples :: Integral a => a -> a
+sumMultiples 0 = 0
+sumMultiples n | n `mod` 3 == 0 = n + sumMultiples (n - 1)
+               | n `mod` 5 == 0 = n + sumMultiples (n - 1)
+               | otherwise      = sumMultiples (n - 1)
+
+main :: IO ()
+main = print $ sumMultiples 999
