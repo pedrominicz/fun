3c6347a4557723c399080f730cab200f653a6335
Pedro Minicz <pedrominicz@gmail.com>
2019-09-01 17:14:43 -0300

    .

--- haskell/rosalind/fib.hs
+++ haskell/rosalind/fib.hs
@@ -4,5 +4,5 @@ fib k = (map fib' [0..] !!)
         fib' n = fib k (n - 1) + k * fib k (n - 2)
 
 main = do
-  [n, k] <- map read <$> words <$> getLine
+  [n, k] <- map read . words <$> getLine
   print $ fib k n
