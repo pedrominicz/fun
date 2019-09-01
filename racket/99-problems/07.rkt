622f8c3893f726d88055b492dcaf190dc8ab467f
Pedro Minicz <pedrominicz@gmail.com>
2019-09-01 14:41:15 -0300

    .

--- racket/99-problems/07.rkt
+++ racket/99-problems/07.rkt
@@ -1,1 +1,12 @@
 #lang racket
+
+(define (my-flatten a)
+  (cond
+    [(empty? a) empty]
+    [(list? (first a)) (append (my-flatten (first a)) (my-flatten (rest a)))]
+    [else (cons (first a) (my-flatten (rest a)))]))
+
+(equal? (my-flatten '()) (flatten '()))
+(equal? (my-flatten '(1 2 3)) (flatten '(1 2 3)))
+(equal? (my-flatten '(1 2 (3 4 5))) (flatten '(1 2 (3 4 5))))
+(equal? (my-flatten '('(1) 2 (3 4 5))) (flatten '('(1) 2 (3 4 5))))
